3913eb1905cd634e45157fe5eb3db1c0e7bef267
HERICK <HERICK@HERICK-PC>
2015-08-14 21:02:02 -0300

    inclusão do metodo main

--- src/main/java/br/senac/tads/pi3/herick/exercicio01/Exercicio01.java
+++ src/main/java/br/senac/tads/pi3/herick/exercicio01/Exercicio01.java
@@ -12,6 +12,7 @@ package br.senac.tads.pi3.herick.exercicio01;
 public class Exercicio01 {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+        
     }
     
 }
